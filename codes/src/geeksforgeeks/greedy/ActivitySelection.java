176e1705a90138034c63331f11c2831a44168fd4
Jilin Meng <jilinm@microsoft.com>
2017-12-19 14:09:16 -0800

    add greedy

---
+++ codes/src/geeksforgeeks/greedy/ActivitySelection.java
@@ -0,0 +1,24 @@
+package geeksforgeeks.greedy;
+
+import java.util.Arrays;
+
+public class ActivitySelection {
+
+	public static void main(String[] args) {
+		int s[] =  {1, 3, 0, 5, 8, 5};
+	    int t[] =  {2, 4, 6, 7, 9, 9};
+	    int[] r = activitySelection(s, t);
+	    int[] exp = {0, 1, 3, 4};
+	    System.out.println(Arrays.compare(r, exp) == 0);
+	}
+	
+	static int[] activitySelection(int[] s, int[] t) {
+		int[] r = new int[s.length];
+		int rc = 1;
+		for (int i = 1; i < s.length; i++) {
+			if (s[i] >= t[r[rc-1]]) r[rc++] = i;
+		}
+		return Arrays.copyOf(r, rc);
+	}
+
+}
